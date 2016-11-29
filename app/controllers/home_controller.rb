a1b135467db1092c2e8cdee37a1e6a4a5f25984e
Ben <eekej001@gmail.com>
2016-11-28 17:57:13 -0800

    changed redirect url to emanga

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
 
   	if params[:id]
-    	redirect_to "https://www.google.com"
+    	redirect_to "https://www.emanga.com"
     else	
    		 @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
    	end
