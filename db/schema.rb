6224ab8d7c2a08867005aec935db4c4509c4d033
Ben <eekej001@gmail.com>
2017-05-03 15:37:35 -0700

    ran - heroku run rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107232640) do
+ActiveRecord::Schema.define(version: 20170503213735) do
+
+  create_table "forms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string   "shopify_domain", limit: 255, null: false
