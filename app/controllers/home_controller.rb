3d70f6a8ca3bdcb5a464f198c7edfb02b87c1dda
Ben <eekej001@gmail.com>
2017-05-08 16:15:03 -0700

    added form save code to proxy action. form submission to ph.com/proxy led to emanga redirect, so the app proxy url may have to match the app action name.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -24,7 +24,15 @@ class HomeController < ShopifyApp::AuthenticatedController
 
 
   def proxy
-    redirect_to "https://www.emanga.com"
+    #redirect_to "https://www.emanga.com"
+    @form = Form.new(form_params)
+    if @form.save
+      flash[:notice] = "Form saved successfully."
+      redirect_to(:action => 'index')
+    else
+      flash[:notice] = "Form save unsuccessful."
+      redirect_to(:action => 'index')   
+    end
   end 	
 
 
