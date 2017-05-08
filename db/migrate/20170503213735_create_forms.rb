24e9b1c5584649ac074aeebd50b946446446ce11
Ben <eekej001@gmail.com>
2017-05-08 14:49:11 -0700

    defined up/down migration for create_forms.rb in migrate folder

--- db/migrate/20170503213735_create_forms.rb
+++ db/migrate/20170503213735_create_forms.rb
@@ -1,8 +1,13 @@
 class CreateForms < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :forms do |t|
-
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.timestamps null: false
     end
+  end
+
+  def self.down
+  	drop_table :forms
   end
 end
