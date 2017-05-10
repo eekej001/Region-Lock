c50d7d4eb7c229e5605e583613bb34ea4e55f4ca
Ben <eekej001@gmail.com>
2017-05-09 17:06:23 -0700

    reinstated shopify authentication on home controller. added proxy controller without shopify authentication.

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,5 +1,5 @@
-class HomeController < ShopifyApp::AuthenticatedController
-  skip_before_action :verify_authenticity_token
+class ProxyController < ApplicationController
+ skip_before_action :verify_authenticity_token
   def index
 
   	#if params[:id]
