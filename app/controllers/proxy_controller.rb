ab8b21cabb4b7c32aff8dba0f796cf444760961d
Ben <eekej001@gmail.com>
2017-05-12 13:36:31 -0700

    added send view file and modified send action of proxy controller with content-type specified

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -29,8 +29,9 @@ class ProxyController < ApplicationController
      #@filename = "#{Rails.root}/public/signup.html"
      #send_file @filename, :type => 'text/html', :x_sendfile=> true
 
+     render layout: false, content_type: 'application/liquid'
 
-     render('index')
+     #render('index')
 
 
 
