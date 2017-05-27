3710f3c1bd2478cc19646ba114d8343f49d44111
Ben <eekej001@gmail.com>
2017-05-26 21:57:15 -0700

    commented out first name json capture

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -61,7 +61,7 @@ class ProxyController < ApplicationController
   def webhook
   	require 'json'
   	#webhook_json = JSON.parse request.body.read
-  	fn = JSON.parse(params[:billing_address][:first_name])
+  	#fn = JSON.parse(params[:billing_address][:first_name])
   	ln = JSON.parse(params[:billing_address][:last_name])
   	#@fn = webhook_json["billing_address"][0]["first_name"]
   	#@ln = webhook_json["billing_address"][0]["last_name"]
