d747a5b8a5aa2a8edd0115a9ef789fb96d23cc04
Ben <eekej001@gmail.com>
2017-05-31 17:48:09 -0700

    specified property of database object

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -28,7 +28,7 @@ class ProxyController < ApplicationController
     fname = params[:username]
     matchup = Form.where(:first_name => '#{fname}').first
     #matchup = Form.where(:first_name => "IT3").first
-    matchupS = matchup.to_s
+    matchupS = matchup.last_name.to_s
     render :json => matchupS
    # redirect_to(:action => 'index') 
 
