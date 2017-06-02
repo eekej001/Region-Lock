b9f0042925b2160d45f2479a86ece39cc2f74000
Ben <eekej001@gmail.com>
2017-06-02 12:44:16 -0700

    removed second origin url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ def add_cors_headers
     origin = "https://projecthentai.com"
     origin2 = "https://www.projecthentai.com"
   end
-  headers['Access-Control-Allow-Origin'] = origin, origin2
+  headers['Access-Control-Allow-Origin'] = origin
   headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT, DELETE'
   allow_headers = request.headers["Access-Control-Request-Headers"]
   if allow_headers.nil?
