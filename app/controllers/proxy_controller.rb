2780ff0465b8f57304c778195b16967245eb4bd8
Ben <eekej001@gmail.com>
2017-06-10 21:27:58 -0700

    sending image_array

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -99,8 +99,8 @@ class ProxyController < ApplicationController
 =end
     #@image_array_json = @image_array.to_json
 
-    #send_data @image_array, :type => 'text/plain', disposition: "inline", :x_sendfile=> true
-    send_data @test_array, :type => 'text/plain', disposition: "inline", :x_sendfile=> true
+    send_data @image_array, :type => 'text/plain', disposition: "inline", :x_sendfile=> true
+    #send_data @test_array, :type => 'text/plain', disposition: "inline", :x_sendfile=> true
 
     #send_data @image_array, :type => 'image/gif', disposition: "inline", :x_sendfile=> true
     #render :json => @image_array_json
