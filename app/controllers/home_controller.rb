5bca58b89d591d4db2a5c3220d7c46c3e3a0ff86
Ben <eekej001@gmail.com>
2017-07-06 15:13:28 -0700

    added doujinshi form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,7 @@ class HomeController < ShopifyApp::AuthenticatedController
    # else	
    		 @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
        @users = Form.all
+       @doujinshis = Doujinshi.all
   #	end
      
      #filename = File.join(Rails.root, "/public/signup.html")
@@ -16,15 +17,15 @@ class HomeController < ShopifyApp::AuthenticatedController
 
 
   def create
-   # @form = Form.new(form_params)
-   # if @form.save
-   #   flash[:notice] = "Form saved successfully."
-   #   redirect_to(:action => 'index')
-   # else
-   #   flash[:notice] = "Form save unsuccessful."
-   #   redirect_to(:action => 'index')   
-   # end  
-#####    #redirect_to(:action => 'index') 
+    @form = Form.new(form_params)
+    if @form.save
+      flash[:notice] = "Form saved successfully."
+      redirect_to(:action => 'index')
+    else
+      flash[:notice] = "Form save unsuccessful."
+      redirect_to(:action => 'index')   
+    end  
+    redirect_to(:action => 'index') 
 
 
 =begin
@@ -38,12 +39,27 @@ class HomeController < ShopifyApp::AuthenticatedController
       end  
 =end
 
+=begin
+
      Form.create(:first_name => "No", :last_name => "Ifs")
      flash[:notice] = "No ifs ands or buts about it"
      redirect_to(:action => 'index')
 
+=end
 
 
+  end
+
+
+  def d_create
+    @douj = Doujinshi.new(douj_params)
+    if @form.save
+      flash[:notice] = "Doujinshi Title Added"
+      redirect_to(:action => 'index')
+    else
+      flash[:notice] = "Doujinshi Title Was Not Added"
+      redirect_to(:action => 'index')   
+    end  
 
   end
 
@@ -61,4 +77,8 @@ private
 
     def form_params
       params.require(:form).permit(:first_name, :last_name)
+    end
+
+    def douj_params
+      params.require(:doujinshi).permit(:title, :handle)
     end
