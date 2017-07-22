d9c22e3aa4daa0882d3d056c9a287da15a4afee6
Ben <eekej001@gmail.com>
2017-07-21 19:25:30 -0700

    changed code to check if active record object persists

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -263,14 +263,14 @@ class ProxyController < ApplicationController
 
 	  for c in title_array do 
 	  	puts "Starting Order History Check"
-	  	#exist_check = Order.where(:first_name => "#{fn}", :last_name => "#{ln}", :email => "#{email}", :title => c)
-	  	if Order.where(:first_name => "#{fn}", :last_name => "#{ln}", :email => "#{email}", :title => c) == nil
-	  	#if exist_check.nil?
-	  		puts "Order does not exist. Time to create."
-	  	   Order.create(:first_name => "#{fn}", :last_name => "#{ln}", :email => "#{email}", :title => c)
+	  	exist_check = Order.where(:first_name => "#{fn}", :last_name => "#{ln}", :email => "#{email}", :title => c)
+	  	#if Order.where(:first_name => "#{fn}", :last_name => "#{ln}", :email => "#{email}", :title => c) == nil
+	  	if exist_check.persisted?
+	  		puts "Order exists so order was not created."
+	  	   puts Order.where(:first_name => "#{fn}", :last_name => "#{ln}", :email => "#{email}", :title => c)   
 	  	else
-	  	   puts "Order was not nil so order was not created."
-	  	   puts Order.where(:first_name => "#{fn}", :last_name => "#{ln}", :email => "#{email}", :title => c)    
+	  	    puts "Order does not exist. Time to create."
+	  	   Order.create(:first_name => "#{fn}", :last_name => "#{ln}", :email => "#{email}", :title => c)
 	  	end   
 	  end  
        puts "Ending Order History Check"
