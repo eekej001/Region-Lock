c3d8d7fcfab60dfa2595949161cd10c55037cb8d
Ben <eekej001@gmail.com>
2017-08-04 12:08:56 -0700

    added title update method

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -2,6 +2,30 @@ class TitlesController < ApplicationController
 
  def index
       @doujinshis = Doujinshi.all
-    end
+ end
 
+ def edit
+    @doujinshi = Doujinshi.find(params[:id])
+ end
+
+
+ def update
+    @doujinshi = Doujinshi.find(params[:id])
+    if @doujinshi.update_attributes(title_params)
+      flash[:notice] = "Title updated successfully."
+      redirect_to(:action => 'index')
+    else  
+      render('edit')
+    end  
+  end 
+
+
+
+  private 
+
+    def title_params
+      params.require(:doujinshi).permit(:title) 
+    end 
+
+ 
 end
