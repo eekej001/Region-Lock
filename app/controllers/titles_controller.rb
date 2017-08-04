1c1dfde3e0f59937ad6f0bf87729039f8e5bf313
Ben <eekej001@gmail.com>
2017-08-04 15:28:59 -0700

    added delete method

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -19,7 +19,15 @@ class TitlesController < ApplicationController
     end  
   end 
 
+def delete
+    @doujinshi = Doujinshi.find(params[:id])
+ end
 
+ def destroy
+    doujinshi = Doujinshi.find(params[:id]).destroy
+    flash[:notice] = "The title '#{doujinshi.title}' has been successfully deleted."
+    redirect_to(:action => 'index')
+  end
 
   private 
 
