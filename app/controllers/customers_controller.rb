39c2ba14972fa17ad42a5ee5193f3a843af5fcfc
Ben <eekej001@gmail.com>
2017-08-07 13:05:02 -0700

    filtering out duplicate entries in customers list by email

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,7 @@
 class CustomersController < ApplicationController
 
  def index
- 	#@customers = Order.all
+ 	@customers = Order.uniq{ |x| x.email }
       #@customers = Order.sorted.uniq{|x| x.email }
  end
 
