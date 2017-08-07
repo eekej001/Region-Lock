f31cdce786206c953665ef15a7dab61cb4c350cb
Ben <eekej001@gmail.com>
2017-08-07 14:04:06 -0700

    added view orders section

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,4 +6,42 @@ class CustomersController < ApplicationController
  end
 
 
+ def orders
+    @orders = Order.find(params[:email])
+ end
+
+
+def edit
+    @order = Order.find(params[:id])
+ end
+
+
+ def update
+    @order = Order.find(params[:id])
+    if @order.update_attributes(order_params)
+      flash[:notice] = "Order updated successfully."
+      redirect_to(:action => 'index')
+    else  
+      render('edit')
+    end  
+  end 
+
+def delete
+    @order = Order.find(params[:id])
+ end
+
+ def destroy
+    order = Order.find(params[:id]).destroy
+    flash[:notice] = "The order has been successfully deleted."
+    redirect_to(:action => 'index')
+  end
+
+  private 
+
+    def order_params
+      params.require(:order).permit(:first_name, :last_name, :email, :title) 
+    end 
+
+
+
 end
