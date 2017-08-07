922d4b2dfd0e545e83d8c4ca17ed305ddf51de67
Ben <eekej001@gmail.com>
2017-08-07 12:31:55 -0700

    added scope to Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@ class Order < ActiveRecord::Base
 	validates_presence_of :last_name
 	validates_presence_of :email
 	validates_presence_of :title
+
+	scope :sorted, lambda { order("title ASC")}    
 end
