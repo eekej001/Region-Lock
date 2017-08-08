f4a9824dd7bc8c076fbab3694383a03447843a1e
Ben <eekej001@gmail.com>
2017-08-07 17:46:08 -0700

    added code to redirect to customer order list

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -14,7 +14,7 @@ class CustomersController < ApplicationController
     @order = Order.new(order_new_params)
     if @order.save
       flash[:notice] = "Doujinshi Title Added To Library"
-      redirect_to(action: 'orders', email: "onemangatwomanga@gmail.com")
+      redirect_to(action: 'orders', email: @order.email)
      # redirect_to(action: 'orders', email: params[:email])
     else
       flash[:notice] = "Doujinshi Title Was Not Added to this Customer's Library"
